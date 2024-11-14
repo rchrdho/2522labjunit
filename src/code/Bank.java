15ba2da83be2cf64dbff1029ad06be02f72d00dc
HoRichard <richardho16@hotmail.com>
2024-11-14 09:58:09 -0800

    iml out to project level

--- src/code/Bank.java
+++ src/code/Bank.java
@@ -5,11 +5,6 @@ public class Bank
 {
     private final BankAccount bankAccount;
 
-    Bank(final BankAccount bankAccount)
-    {
-        this.bankAccount = bankAccount;
-    }
-
     public void addAccount(final BankAccount bankAccount)
     {
 
