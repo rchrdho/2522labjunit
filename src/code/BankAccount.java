cb7c61c6e7b1c10ffe3bfc6843db275c14b8802b
rchrdho <richardho16@hotmail.com>
2024-11-16 18:20:20 -0800

    error handling, almost done bankaccount will continue tomorrow

--- src/code/BankAccount.java
+++ src/code/BankAccount.java
@@ -6,12 +6,16 @@ public class BankAccount
     BankAccount(final String accountNumber,
                 final double balance)
     {
+        isValidAccountNumber(accountNumber);
+
         this.accountNumber = accountNumber;
         this.balance       = balance;
     }
 
     public void deposit(final double depositAmount)
     {
+        isValidDepositAmount(depositAmount);
+
         balance += depositAmount;
     }
 
@@ -22,18 +26,60 @@ public class BankAccount
 
     public final void withdraw(final double withdrawAmount)
     {
+        isValidWithdrawAmount(withdrawAmount);
+
         balance -= withdrawAmount;
     }
 
-    public void transferToBank(final BankAccount account,
-                               final String accountNumber,
-                               final int bankId)
+    public final void transferToBank(final BankAccount account,
+                                     final String      accountNumber,
+                                     final int         amount)
     {
+        isMatchingAccountNumber(accountNumber);
 
+        this.deposit(amount);
+    }
+
+    private void isValidAccountNumber(final String accountNumber)
+    {
+        boolean emptyAccountNum = accountNumber.isBlank();
+
+        if(accountNumber.isEmpty() && !emptyAccountNum)
+        {
+            throw new IllegalArgumentException("Invalid Account Number");
+        }
+    }
+
+    private void isMatchingAccountNumber(final String accountNumber)
+    {
+        isValidAccountNumber(accountNumber);
+
+        if(!accountNumber.equalsIgnoreCase(this.accountNumber))
+        {
+            throw new IllegalArgumentException("Account not found");
+        }
+    }
+
+    private void isValidDepositAmount(final double depositAmount)
+    {
+        if(depositAmount < 0)
+        {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+    }
+
+    private void isValidWithdrawAmount(final double withdrawAmount)
+    {
+        if(balance < withdrawAmount)
+        {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
     }
 
     public static void main(final String[] args)
     {
+        BankAccount account;
 
+        account = new BankAccount("12312", 23);
     }
 }
